f48e412422d326767e74ed8d6d58c704e1175e5c
qianchd <qianchd@gmail.com>
2023-03-27 18:38:02 +0800

    add E and F button

--- overleaf.user.js
+++ overleaf.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Beautify overleaf
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  beautiful overleaf
 // @author       You
 // @match        https://www.overleaf.com/*
@@ -10,35 +10,35 @@
 // ==/UserScript==
 function addStyle() {
     let css = `
-        ::-webkit-scrollbar {
-        width: 4px;
-        height: 6px;
-        background-color: transparent;
-        border-style: none;
-        }
+    ::-webkit-scrollbar {
+    width: 4px;
+    height: 6px;
+    background-color: transparent;
+    border-style: none;
+}
 
-        ::-webkit-scrollbar-track:hover {
-            background-color: rgba(0, 0, 0, .1);
-        }
+::-webkit-scrollbar-track:hover {
+    background-color: rgba(0, 0, 0, .1);
+}
 
-        ::-webkit-scrollbar-thumb,
-        ::-webkit-scrollbar-thumb {
-            background-clip: border-box;
-            background-color: rgba(0, 0, 0, .2);
-        }
+::-webkit-scrollbar-thumb,
+::-webkit-scrollbar-thumb {
+    background-clip: border-box;
+    background-color: rgba(0, 0, 0, .2);
+}
 
-        ::-webkit-scrollbar-thumb:hover {
-            background-color: rgba(0, 0, 0, .3);
-        }
+::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(0, 0, 0, .3);
+}
 
-        ::-webkit-scrollbar-thumb:active {
-            background-color: rgba(0, 0, 0, .5);
-        }
+::-webkit-scrollbar-thumb:active {
+    background-color: rgba(0, 0, 0, .5);
+}
 
-        botton {
-            font-size: 4px;
-        }
-        `;
+botton {
+    font-size: 4px;
+}
+`;
 
     GM_addStyle(css);
 };
@@ -47,10 +47,49 @@ function addStyle() {
 (function() {
     'use strict';
 
+    // Your code here...
     setTimeout(function () {
     var obj=document.getElementsByClassName("cm-gutter-lint")[0];
     obj.remove();
     console.log(obj);
+                var button3 = document.createElement("button");
+        button3.id = "id003";
+        button3.textContent = "F";
+        button3.style.width = "38px";
+        button3.style.height = "38px";
+        button3.style.border = "none";
+        button3.style.align = "center";
+        button3.onclick = function () {
+            let element = document.documentElement;
+                if(element.requestFullscreen) {
+                    element.requestFullscreen();
+                } else if(element.webkitRequestFullScreen) {
+                    element.webkitRequestFullScreen();
+                } else if(element.mozRequestFullScreen) {
+                    element.mozRequestFullScreen();
+                } else if(element.msRequestFullscreen) {
+                    // IE11
+                    element.msRequestFullscreen();
+                }
+        }
+        var button4 = document.createElement("button");
+        button4.id = "id004";
+        button4.textContent = "E";
+        button4.style.width = "38px";
+        button4.style.height = "38px";
+        button4.style.border = "none";
+        button4.style.align = "center";
+        button4.onclick = function () {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
         var button2 = document.createElement("button");
         button2.id = "id002";
         button2.textContent = "L";
@@ -66,11 +105,11 @@ function addStyle() {
             else{
                 obj.style.display="none";
             }
-            
+
         }
         var button = document.createElement("button");
         button.id = "id001";
-        button.textContent = "F";
+        button.textContent = "H";
         button.style.width = "38px";
         button.style.height = "38px";
         button.style.border = "none";
@@ -81,8 +120,8 @@ function addStyle() {
             let pane=document.getElementsByClassName("ui-layout-pane")
             // tb.remove();
             if(tb.style.display=="none"){
-                tb.style.display='flex';
-                main.style.top = "40";
+                // tb.style.display='flex';
+                // main.style.top = "40";
             }
             else{
                 tb.style.display="none";
@@ -93,12 +132,14 @@ function addStyle() {
             }
 		    return;
 	    };
-        // var x = document.getElementsByClassName('project-name')[0];
+        var x = document.getElementsByClassName('project-name')[0];
 
-        // var onuser = document.getElementsByClassName('online-user')[1];
+        var onuser = document.getElementsByClassName('online-user')[1];
         var tabedit = document.getElementsByClassName('toolbar-editor')[0];
         tabedit.appendChild(button2);
         tabedit.appendChild(button);
+        tabedit.appendChild(button3);
+        tabedit.appendChild(button4);
         addStyle();
-}, 3000);
+}, 5000);
 })();
